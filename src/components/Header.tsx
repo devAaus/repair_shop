c9c7162eb6bda17ecff55200c8104e59eadc6b0f
Aayush <aausgh@gmail.com>
2024-12-04 17:06:27 +0545

    login auth and middleware

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { NavButton } from './NavButton'
-import { File, HomeIcon, UsersRound } from 'lucide-react'
+import { File, HomeIcon, LogOut, UsersRound } from 'lucide-react'
 import Link from 'next/link'
 import { ModeToggle } from './ModeToggle'
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components'
+import { Button } from './ui/button'
 
 export const Header = () => {
    return (
@@ -19,7 +21,18 @@ export const Header = () => {
                <NavButton href='/tickets' label='Tickets' icon={File} />
                <NavButton href='/customers' label='Customers' icon={UsersRound} />
                <ModeToggle />
-
+               <Button
+                  variant="ghost"
+                  size="icon"
+                  aria-label='LogOut'
+                  title="LogOut"
+                  className='rounded-full'
+                  asChild
+               >
+                  <LogoutLink>
+                     <LogOut />
+                  </LogoutLink>
+               </Button>
             </div>
          </div>
       </header>
